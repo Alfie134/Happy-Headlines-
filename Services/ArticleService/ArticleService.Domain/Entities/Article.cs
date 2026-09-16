e94d6db550715f41a6daf9a79055b060f917f6e7
Alfie134 <alberteoegendahl@gmail.com>
2026-09-16 14:32:10 +0100

    references added again, nice and program.cs updated

--- Services/ArticleService/ArticleService.Domain/Entities/Article.cs
+++ Services/ArticleService/ArticleService.Domain/Entities/Article.cs
@@ -2,10 +2,10 @@ namespace ArticleService.Domain.Entities;
 
 public class Article
 {
-    public int ArticleId { get; set; }
+    public Guid ArticleId { get; set; }
     public string Title { get; set; }
+    public string Content { get; set; }
     public string Author { get; set; }
     public DateOnly PublicationDate { get; set; }
-    public string Content { get; set; }
 
 }
--- Services/DraftService/DraftService.Api/Program.cs
+++ Services/DraftService/DraftService.Api/Program.cs
@@ -1,41 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Services.DraftService.DraftService.Infrastructure.Data;
+
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Service", "DraftService")
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341")
+    .CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+builder.Services.AddDbContext<DraftDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DraftDbContext")));
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
